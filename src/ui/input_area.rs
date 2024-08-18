ed35ea9f626115345c312bf1a312f87a933397ab
ddxfish <ddxfish@gmail.com>
2024-08-18 02:16:40 -0400

    0.22 stable mostly

--- src/ui/input_area.rs
+++ src/ui/input_area.rs
@@ -6,15 +6,16 @@ pub fn render_input(ui: &mut Ui, chat: &mut Chat, icons: &Icons, input: &mut Str
     let input_field = TextEdit::multiline(input)
         .desired_rows(3)
         .hint_text("Type your message here...")
-        .font(egui::FontId::proportional(14.0));
+        .font(egui::FontId::proportional(16.0));
 
     let response = ui.add_sized(
         [ui.available_width(), 50.0],
         input_field
     );
+    //ui.add_space(50.0);
 
-    let button_size = Vec2::new(25.0, 25.0);
-    let button_pos = ui.min_rect().right_bottom() - button_size - Vec2::new(5.0, 42.0);
+    let button_size = Vec2::new(20.0, 20.0);
+    let button_pos = ui.min_rect().right_bottom() - button_size - Vec2::new(12.0, 38.0);
     
     if ui.put(egui::Rect::from_min_size(button_pos, button_size), Button::image(Image::new(&icons.send).fit_to_exact_size(button_size))).clicked()
        || (ui.input(|i| i.key_pressed(egui::Key::Enter) && !i.modifiers.shift)) {
