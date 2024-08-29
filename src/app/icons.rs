8a11022e169f265160ebaf3846260abbc6eadad0
ddxfish <ddxfish@gmail.com>
2024-08-28 23:30:39 -0400

    cleanup 244

--- src/app/icons.rs
+++ src/app/icons.rs
@@ -10,19 +10,19 @@ pub struct Icons {
 impl Icons {
     pub fn new(ctx: &egui::Context) -> Self {
         Self {
-            send: load_icon_texture(ctx, "assets/paper-plane-regular-white.png"),
-            close: load_icon_texture(ctx, "assets/close_icon.png"),
-            new_chat: load_icon_texture(ctx, "assets/app_icon.png"),
-            trash: load_icon_texture(ctx, "assets/trash_icon.png"),
+            send: load_icon_texture(ctx, "send", include_bytes!("../../assets/paper-plane-regular-white.png")),
+            close: load_icon_texture(ctx, "close", include_bytes!("../../assets/close_icon.png")),
+            new_chat: load_icon_texture(ctx, "new_chat", include_bytes!("../../assets/app_icon.png")),
+            trash: load_icon_texture(ctx, "trash", include_bytes!("../../assets/trash_icon.png")),
         }
     }
 }
 
-fn load_icon_texture(ctx: &egui::Context, path: &str) -> TextureHandle {
-    let image = image::open(path).expect("Failed to open icon").into_rgba8();
+fn load_icon_texture(ctx: &egui::Context, name: &str, bytes: &[u8]) -> TextureHandle {
+    let image = image::load_from_memory(bytes).expect("Failed to load icon").to_rgba8();
     let (width, height) = image.dimensions();
     ctx.load_texture(
-        path,
+        name,
         egui::ColorImage::from_rgba_unmultiplied([width as _, height as _], &image),
         egui::TextureOptions::default()
     )
